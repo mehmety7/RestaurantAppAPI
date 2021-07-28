37c3069fef0384f76970bda7895e8e7487795ab5
Mehmet Yazıcı <mehmetyazici.3407@gmail.com>
2021-07-28 09:52:48 +0300

    testing issues

--- src/test/java/com/finartz/restaurantapp/service/AddressServiceTest.java
+++ src/test/java/com/finartz/restaurantapp/service/AddressServiceTest.java
@@ -3,7 +3,7 @@ package com.finartz.restaurantapp.service;
 import com.finartz.restaurantapp.model.Address;
 import com.finartz.restaurantapp.repository.AddressRepository;
 import org.junit.Test;
-import org.junit.jupiter.api.Assertions;
+import org.mockito.ArgumentMatchers;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,6 +11,8 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 
 import java.util.Arrays;
 import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 public class AddressServiceTest {
@@ -22,15 +24,25 @@ public class AddressServiceTest {
     private AddressRepository addressRepository;
 
     @Test
-    public void whenFetchAll_thenReturnAllAddress() throws Exception {
+    public void whenFetchAll_thenReturnAllAddress() {
         Address address1 = Address.builder().id(1l).name("Ev").build();
         Address address2 = Address.builder().id(2l).name("İş").build();
 
-        List<Address> addressList = Arrays.asList(address1, address1);
+        List<Address> addressList = Arrays.asList(address1, address2);
         Mockito.when(addressRepository.findAll()).thenReturn(addressList);
 
         List<Address> fetchedList = addressService.getAll();
-        Assertions.assertEquals(addressList.size(), fetchedList.size());
+        assertEquals(addressList.size(), fetchedList.size());
+    }
+
+    @Test
+    public void whenAddAddress_thenReturnSavedRecord() {
+        Address address = Address.builder().name("Ev").build();
+        Mockito.doReturn(address).when(addressRepository).save(ArgumentMatchers.any());
+
+        Address returnedAddress = addressService.create(address);
+
+        assertEquals(address.getName(), returnedAddress.getName());
     }
 
 }
