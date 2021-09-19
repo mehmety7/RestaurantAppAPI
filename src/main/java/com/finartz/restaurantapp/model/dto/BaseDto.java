efafa3256743ca5a7a9b6d00ca787d7f7f1070bb
Mehmet Yazıcı <mehmetyazici.3407@gmail.com>
2021-09-19 23:54:34 +0300

    add Serializable impl

---
+++ src/main/java/com/finartz/restaurantapp/model/dto/BaseDto.java
@@ -0,0 +1,9 @@
+package com.finartz.restaurantapp.model.dto;
+
+import java.io.Serializable;
+
+public class BaseDto implements Serializable {
+
+    private final static long serialVersionUID = 2316546546L;
+
+}
