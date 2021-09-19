42fcbe553005b72db03e176d19b299aaa3b91455
Mehmet Yazıcı <mehmetyazici.3407@gmail.com>
2021-09-19 23:55:35 +0300

    add Serializable impl

---
+++ src/main/java/com/finartz/restaurantapp/model/request/BaseRequest.java
@@ -0,0 +1,9 @@
+package com.finartz.restaurantapp.model.request;
+
+import java.io.Serializable;
+
+public class BaseRequest implements Serializable {
+
+    private final static long serialVersionUID = 64987687L;
+
+}
