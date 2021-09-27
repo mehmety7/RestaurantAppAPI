be03d6b7a8732d0c8d04d7f8ecf52b7513a1a8d5
Mehmet Yazıcı <mehmetyazici.3407@gmail.com>
2021-09-27 21:23:20 +0300

    fixed test bugs

--- src/test/java/com/finartz/restaurantapp/RestaurantappApplicationTests.java
+++ src/test/java/com/finartz/restaurantapp/RestaurantappApplicationTests.java
@@ -2,14 +2,16 @@ package com.finartz.restaurantapp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootTest
+@EnableCaching
 class RestaurantappApplicationTests {
 
 	@Test
 	void contextLoads() {
-		String[] args = new String[0];
-		RestaurantappApplication.main(args);
+//		String[] args = new String[0];
+//		RestaurantappApplication.main(args);
 	}
 
 }
