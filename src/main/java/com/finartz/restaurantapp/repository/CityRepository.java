123b31ee39a7a6f4b6d19ffa26d427b6cc353178
Mehmet Yazıcı <mehmetyazici.3407@gmail.com>
2021-09-27 16:21:19 +0300

    Solution N+1 Selects Problem in Fetch Cities and their Counties

--- src/main/java/com/finartz/restaurantapp/repository/CityRepository.java
+++ src/main/java/com/finartz/restaurantapp/repository/CityRepository.java
@@ -1,8 +1,15 @@
 package com.finartz.restaurantapp.repository;
 
 import com.finartz.restaurantapp.model.entity.CityEntity;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface CityRepository extends BaseRepository<CityEntity>{
+
+    @Query("SELECT c FROM CityEntity c LEFT JOIN FETCH c.countyEntities")
+    List<CityEntity> findAll();
+
 }
