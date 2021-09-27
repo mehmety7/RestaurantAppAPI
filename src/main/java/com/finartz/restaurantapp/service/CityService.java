2882bfc76398517416896f304140787a47f86f90
Mehmet Yazıcı <mehmetyazici.3407@gmail.com>
2021-09-27 16:22:13 +0300

    add spring caching operation

--- src/main/java/com/finartz/restaurantapp/service/CityService.java
+++ src/main/java/com/finartz/restaurantapp/service/CityService.java
@@ -1,15 +1,18 @@
 package com.finartz.restaurantapp.service;
 
 import com.finartz.restaurantapp.model.dto.CityDto;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.Cacheable;
 
 import java.util.List;
 
+@CacheConfig(cacheNames = {"cities"})
 public interface CityService {
 
+    @Cacheable
     CityDto getCity(Long id);
 
+    @Cacheable
     List<CityDto> getCities();
-
-
 
 }
--- src/main/java/com/finartz/restaurantapp/service/CountyService.java
+++ src/main/java/com/finartz/restaurantapp/service/CountyService.java
@@ -1,13 +1,18 @@
 package com.finartz.restaurantapp.service;
 
 import com.finartz.restaurantapp.model.dto.CountyDto;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.Cacheable;
 
 import java.util.List;
 
+@CacheConfig(cacheNames = {"counties"})
 public interface CountyService {
 
+    @Cacheable
     CountyDto getCounty(Long id);
 
+    @Cacheable
     List<CountyDto> getCounties(Long city_id);
 
 }
